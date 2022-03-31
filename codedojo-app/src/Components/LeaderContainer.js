34ebad6107717c4d0d23ecb0ac5c529b18308ede
Ryan O'Neill <ryanponeill00@gmail.com>
2022-03-31 14:29:33 -0400

    final

--- codedojo-app/src/Components/LeaderContainer.js
+++ codedojo-app/src/Components/LeaderContainer.js
@@ -1,26 +1,33 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 
 function LeaderContainer() {
-
-  const [leaderboard, setLeaderboard] = useState([])
+  const [leaderboard, setLeaderboard] = useState([]);
 
   useEffect(() => {
     fetch("/clans")
       .then((r) => r.json())
-      .then(data => setLeaderboard(data));
-}, [])
+      .then((data) => setLeaderboard(data));
+  }, []);
 
-const clanNames = leaderboard.map(c => Object.keys(c))
-const scores = leaderboard.map(c => Object.values(c))
+  const clanNames = leaderboard.map((c) => Object.keys(c));
+  const scores = leaderboard.map((c) => Object.values(c));
 
   return (
     <div>
-        <p><b>{clanNames[0]}</b> has scored {scores[0]} points</p>
-        <p><b>{clanNames[1]}</b> has scored {scores[1]} points</p>
-        <p><b>{clanNames[2]}</b> has scored {scores[2]} points</p>
-        <p><b>{clanNames[3]}</b> has scored {scores[3]} points</p>
+      <p>
+        <b className="b-font">{clanNames[0]}</b> has scored {scores[0]} points
+      </p>
+      <p>
+        <b className="b-font">{clanNames[1]}</b> has scored {scores[1]} points
+      </p>
+      <p>
+        <b className="b-font">{clanNames[2]}</b> has scored {scores[2]} points
+      </p>
+      <p>
+        <b className="b-font">{clanNames[3]}</b> has scored {scores[3]} points
+      </p>
     </div>
-  )
+  );
 }
 
 export default LeaderContainer;
