b926ac1c723562ac697cfdb03741be600eb61d70
Ryan O'Neill <ryanponeill00@gmail.com>
2022-03-31 11:41:52 -0400

    flier image css  on lander page

--- codedojo-app/src/Components/Lander.js
+++ codedojo-app/src/Components/Lander.js
@@ -4,6 +4,13 @@ import { Link } from "react-router-dom";
 function Lander() {
   return (
     <div>
+      <div class="flier">
+        <img
+          src="https://res.cloudinary.com/april-skrine/image/upload/v1648740487/Phase%204%20Project/111-1119422_cool-ninja-star-outline-removebg-preview_ciecig.png"
+          width="200"
+          alt="flier"
+        />
+      </div>
       <div className="lander-container">
         <div className="break"></div>
         <div className="lander-container-child">
